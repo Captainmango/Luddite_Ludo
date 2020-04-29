865d4a03194f9bc34f111c26d2370a80c34659de
Edward Heaver <edward.heaver01@gmail.com>
2020-04-29 17:04:05 +0100

    created fetch request for sign in

--- frontend/index.js
+++ frontend/index.js
@@ -8,7 +8,7 @@
 
 //game class - to handle game logic
 
-
+const base_url = "http//localhost:3000";
 
        
 var openBtn = document.getElementById('signInBtn');
@@ -18,14 +18,40 @@ var signInModal = document.getElementById('signInModal');
 openBtn.addEventListener('click', getSignIn);
 closeBtn.addEventListener('click', closeSignIn);
 
-function closeSignIn(){
+function closeSignIn() {
     signInModal.style.display = "none";
 }
 
 
-function getSignIn(){
-    
+function getSignIn() {
     signInModal.style.display = "block";
+}
+
+function signUp(email, password){
+    let formData = {}
+    formData["email"] = email;
+    formData["password"] = password;
+
+    let confObj = {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: formData
+        }
+    let req_url = base_url + "/users";
+
+    fetch(req_url, confObj).then((response)=>{
+        if(response.ok){
+            signInModal.style.display = "none";
+        } else {
+            signInModal.style.display = "none";
+            alert("Account not created. Check your email and password");
+        }
+
+    })
 
 }
 
