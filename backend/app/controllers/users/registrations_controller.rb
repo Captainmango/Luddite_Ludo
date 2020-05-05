7959d5d07a3e717ee2661ccdf4bee2ab2fc23e01
Edward Heaver <edward.heaver01@gmail.com>
2020-05-05 20:05:31 +0100

    controller request handling

--- backend/app/controllers/users/registrations_controller.rb
+++ backend/app/controllers/users/registrations_controller.rb
@@ -4,11 +4,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
 
     def create
-        nu_user = User.new
-        nu_user.email = params["email"]
-        nu_user.password = params["password"]
-        nu_user.save
-        render json: {nu_user_id: nu_user.id}
+        nu_user = User.find_or_create_by(email: params["email"])
+        if nu_user.valid?
+            render json: {nu_user_id: nu_user.id}
+        else
+            nu_user.password = params["password"]
+            nu_user.save
+            render json: {nu_user_id: nu_user.id,
+                            errors: nu_user.errors}
+        end
+
     end
 
 end
