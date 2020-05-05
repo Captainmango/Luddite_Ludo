6af7ccf5bc38712518776a3204ac442ed6baf8c4
Edward Heaver <edward.heaver01@gmail.com>
2020-05-05 20:43:15 +0100

    wrote log in route

--- backend/app/controllers/users/sessions_controller.rb
+++ backend/app/controllers/users/sessions_controller.rb
@@ -4,6 +4,17 @@ class Users::SessionsController < Devise::SessionsController
 
 
     def create
+        user = User.find_by(email: params["email"])
+        if user.valid?
+            if user.valid_password?(params["password"])
+                render json: {user_id: user.id}.to_json
+            else
+                render json: {user_id: nil}.to_json
+            end
+        else
+            render json: {user_id: nil}
+        end
+    end
 
     end
 
