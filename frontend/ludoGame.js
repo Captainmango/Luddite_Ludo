eb6022e6a4da1c95b5d2988f00fb5bdc1e72de1d
Edward Heaver <edward.heaver01@gmail.com>
2020-05-06 17:38:02 +0100

    wrote resize board function

---
+++ frontend/ludoGame.js
@@ -0,0 +1,11 @@
+window.onresize = resizeboard;
+
+function refreshBoard() {
+    canvasWidth = window.innerHeight - 10;
+    canvasHeight = window.innerHeight;
+    maindiv.style.width = canvasWidth + "px";
+    maindiv.style.height = canvasWidth + "px";
+    ctx.canvas.width = canvasWidth;
+    ctx.canvas.height = canvasWidth;
+    tileWidth = Math.ceil(canvasWidth / 16);
+}
