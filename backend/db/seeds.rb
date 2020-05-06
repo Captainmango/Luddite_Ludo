733518673db3ea65cd539c869f3ed71682b52f8a
Edward Heaver <edward.heaver01@gmail.com>
2020-05-06 17:12:58 +0100

    wrote seed file for test data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+colours = ["red", "blue", "yellow", "green"]
+turn = 1
+
+user = User.create(email: "test@test.com", password: "123456789")
+user.save
+
+nu_game = user.games.new
+nu_game.save
+
+30.times do
+    nu_game.turns.create(colour: colours[0], token: rand(1..4), roll: rand(1..6))
+end
+        
