0255827a58e5d14685dda54e10df6792c38cfaa5
Edward Heaver <edward.heaver01@gmail.com>
2020-05-12 13:21:50 +0100

    refactoring games and turns controller

--- backend/app/controllers/games_controller.rb
+++ backend/app/controllers/games_controller.rb
@@ -1,24 +1,31 @@
 class GamesController < ApplicationController
-
+    before_action :get_game, only: [:show, :destroy]
     respond_to :html, :json
 
     def index
-
+        games = Game.where(user_id: params['user_id'])
+        render :json {games}.to_json
     end
 
     def create
         game = Game.create(user_id: params['current_user'])
         game.save
-        render :json {game_id: game.id, user_email: User.find_by_id(game.user_id).email, user_id: game.user_id}
+        render :json {game_id: game.id, user_email: User.find_by_id(game.user_id).email, user_id: game.user_id}.to_json
     end
 
     def show
-        game = Game.find_by_id(params['game_id'])
-        game_turns = game.turns
+        last_turn = game.turns.last
+        render :json {game, last_turn}.to_json
     end
 
     def destroy
+        Turn.where(game_id: game.id).destroy_all
+        game.destroy
+        render :json {}.to_json
+    end
 
+    def get_game
+        game = Game.find_by_id(params['game_id'])
     end
 
 
--- backend/app/controllers/turns_controller.rb
+++ backend/app/controllers/turns_controller.rb
@@ -1,13 +1,19 @@
 class TurnsController < ApplicationController
-
+    before_action :get_game, only: [:index, :create]
     respond_to :html, :json
 
     def create
-        turn = Turn.create()
+        turn = game.turns.create(colour: params['colour'], pawn: params['pawn'], roll: params['roll'])
+        render :json {turn}.to_json
     end
 
     def index
+        turns = game.turns
+        render :json {turns}.to_json
+    end
 
+    def get_game
+        game = Game.find_by_id(params['game_id'])
     end
 
 end
