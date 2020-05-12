9020af4bd7ca3d4b38f2924c850eee92f46a396b
Edward Heaver <edward.heaver01@gmail.com>
2020-05-12 13:07:06 +0100

    working on game and turn prod logic

--- backend/app/controllers/games_controller.rb
+++ backend/app/controllers/games_controller.rb
@@ -1,2 +1,25 @@
 class GamesController < ApplicationController
+
+    respond_to :html, :json
+
+    def index
+
+    end
+
+    def create
+        game = Game.create(user_id: params['current_user'])
+        game.save
+        render :json {game_id: game.id, user_email: User.find_by_id(game.user_id).email, user_id: game.user_id}
+    end
+
+    def show
+        game = Game.find_by_id(params['game_id'])
+        game_turns = game.turns
+    end
+
+    def destroy
+
+    end
+
+
 end
--- backend/app/controllers/turns_controller.rb
+++ backend/app/controllers/turns_controller.rb
@@ -1,2 +1,13 @@
 class TurnsController < ApplicationController
+
+    respond_to :html, :json
+
+    def create
+        turn = Turn.create()
+    end
+
+    def index
+
+    end
+
 end
