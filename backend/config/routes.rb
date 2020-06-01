5040dac837bec1574f0231d25c715db3d68bb134
Edward Heaver <heaver.edward02@gmail.com>
2020-06-01 18:54:02 +0100

    master test

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {sessions: "sessions",
-                                  registrations: "registrations"
+  devise_for :users, controllers: {sessions: "users/sessions",
+                                  registrations: "users/registrations"
                                   }
   post "/games", to: "games#create"
   get "/games", to: "games#index"
